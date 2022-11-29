160c028a34a63488db40844b19da8332aed26e41
KTCPP <113586816+KTCPP@users.noreply.github.com>
2022-11-29 15:52:35 +0100

    tripled the age instead of doubling

--- cli.cpp
+++ cli.cpp
@@ -3,6 +3,6 @@ int get_age_from_cli() {
 	int myAge;
 	std::cout << "Enter your age: ";
 	std::cin >> myAge;
-	std::cout << (myAge * 2) << std::endl;
+	std::cout << (myAge * 3) << std::endl;
 	return myAge;
 }
